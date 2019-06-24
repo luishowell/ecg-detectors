56bbb0b33e25a12c2a1dd5f9a3134a638624150f
Luis Howell <luisbhowell@gmail.com>
2019-06-24 15:53:41 +0100

    added plotting to analysis

--- results_analysis.py
+++ results_analysis.py
@@ -2,10 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# det_names = ['two_average', 'matched_filter', 'swt', 'engzee', 'christov', 'hamilton', 'pan_tompkins']
-# experiment_names = ['sitting','maths','walking','hand_bike','jogging']
-# output_names = ['TP', 'FP', 'FN', 'TN']
-
 
 def str_join(a, b, c):
 
@@ -66,16 +62,83 @@ def f1(results_df, detector_name, experiments=None):
     return f1_val
 
 
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2.0, 1.01*height,'%.2f' % height,ha='center', va='bottom')
+
+
 gudb_cs_results = pd.read_csv('results_GUDB_chest_strap_11.21.csv', dtype=int, index_col=0)
+gudb_cable_results = pd.read_csv('results_GUDB_loose_cables_15.35.csv', dtype=int, index_col=0)
 mitdb_results = pd.read_csv('results_MITDB_13.49.csv', dtype=int, index_col=0)
 
+det_names = ['two_average', 'matched_filter', 'swt', 'engzee', 'christov', 'hamilton', 'pan_tompkins']
 experiment_names = ['sitting','maths','walking','hand_bike','jogging']
+output_names = ['TP', 'FP', 'FN', 'TN']
 
 
-print(sensitivity(gudb_cs_results, 'two_average', experiment_names))
-print(ppv(gudb_cs_results, 'two_average', experiment_names))
-print(f1(gudb_cs_results, 'two_average', experiment_names))
-print()
-print(sensitivity(mitdb_results, 'two_average'))
-print(ppv(mitdb_results, 'two_average'))
-print(f1(mitdb_results, 'two_average'))
+mitdb_se = []
+for det in det_names:
+    mitdb_se.append(sensitivity(mitdb_results, det))
+mitdb_ppv = []
+for det in det_names:
+    mitdb_ppv.append(ppv(mitdb_results, det))
+mitdb_f1 = []
+for det in det_names:
+    mitdb_f1.append(f1(mitdb_results, det))
+
+gudb_cs_se = []
+for det in det_names:
+    gudb_cs_se.append(sensitivity(gudb_cs_results, det, experiment_names))
+gudb_cs_ppv = []
+for det in det_names:
+    gudb_cs_ppv.append(ppv(gudb_cs_results, det, experiment_names))
+gudb_cs_f1 = []
+for det in det_names:
+    gudb_cs_f1.append(f1(gudb_cs_results, det, experiment_names))
+
+gudb_cable_se = []
+for det in det_names:
+    gudb_cable_se.append(sensitivity(gudb_cable_results, det, experiment_names))
+gudb_cable_ppv = []
+for det in det_names:
+    gudb_cable_ppv.append(ppv(gudb_cable_results, det, experiment_names))
+gudb_cable_f1 = []
+for det in det_names:
+    gudb_cable_f1.append(f1(gudb_cable_results, det, experiment_names))
+
+
+x_pos = np.arange(len(det_names))
+width = 0.4
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x_pos, mitdb_se, align='center')
+ax.set_ylabel('Sensitivity (%)')
+ax.set_title('Detector Sensitivity on MITDB')
+ax.set_xticks(x_pos)
+ax.set_xticklabels(det_names)
+autolabel(rects1)
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x_pos, mitdb_ppv, align='center')
+ax.set_ylabel('PPV (%)')
+ax.set_title('Detector PPV on MITDB')
+ax.set_xticks(x_pos)
+ax.set_xticklabels(det_names)
+autolabel(rects1)
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x_pos, gudb_cs_se, width)
+rects2 = ax.bar(x_pos+width, gudb_cable_se, width)
+ax.set_ylabel('PPV (%)')
+ax.set_title('Detector PPV on GUDB')
+ax.set_xticks(x_pos + width / 2)
+ax.set_xticklabels(det_names)
+ax.legend((rects1[0], rects2[0]), ('Chest Strap', 'Loose Cables'))
+autolabel(rects1)
+autolabel(rects2)
+
+plt.show()
