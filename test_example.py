5da35cc37ff732fabd2a075d9ed3ecca0302d838
Luis Howell <luisbhowell@gmail.com>
2019-07-01 16:38:31 +0100

    updated tester example to reflect recent changes

--- test_example.py
+++ test_example.py
@@ -1,16 +1,30 @@
 import numpy as np
 from tester_MITDB import MITDB_test
+from tester_GUDB import GUDB_test
 from ecgdetectors import Detectors
 
 
-fs = 360
-detectors = Detectors(fs)
-
 # MIT-BIH database testing
-mit_test = MITDB_test(r'\MITDB_dir')
+mit_test = MITDB_test(r'C:\Users\Luis\Documents\MITDB')
+mit_detectors = Detectors(360)
 
-results = mit_test.classifier_test(detectors.matched_filter_detector, tolerance=0, print_results=True)
-np.savetxt('MITDB_results.csv', results, fmt='%i', delimiter=',')
+# test single detector
+matched_filter_mit = mit_test.single_classifier_test(mit_detectors.matched_filter_detector, tolerance=0, print_results=True)
+np.savetxt('matched_filter_mit.csv', matched_filter_mit, fmt='%i', delimiter=',')
 
-Z_value = mit_test.mcnemars_test(detectors.swt_detector, detectors.two_average_detector, print_results = True)
+# test all detectors on MITDB, will save results to csv, will take some time
+mit_results = mit_test.classifer_test_all()
 
+# McNemars stats test
+Z_value_mit = mit_test.mcnemars_test(mit_detectors.swt_detector, mit_detectors.two_average_detector, print_results = True)
+
+
+# GUDB database testing
+gu_test = GUDB_test()
+gu_detectors = Detectors(250)
+
+swt_gudb = gu_test.single_classifier_test(gu_detectors.swt_detector, tolerance=0, config='chest_strap')
+
+gu_results = gu_test.classifer_test_all(tolerance=0, config='chest_strap')
+
+Z_value_gu = gu_test.mcnemars_test(gu_detectors.swt_detector, gu_detectors.two_average_detector, print_results = True)
